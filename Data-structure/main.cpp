005bb0a4bfc850d46e3443a99d34632220a751d0
roadmonster <roadmonster1989@gmail.com>
2021-03-19 23:15:06 -0700

    Initial Commit

---
+++ Data-structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data-structure
+//
+//  Created by HAO LI on 3/19/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
